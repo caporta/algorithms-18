fddf5c39bf1c2dda98e90dafeaeb2c0bb1ccc917
caporta <caporta@users.noreply.github.com>
2018-06-10 19:46:22 -0400

    Refactor function to remove extraneous dict values and set

--- hashes-and-sets/first_uniq_char.py
+++ hashes-and-sets/first_uniq_char.py
@@ -1,14 +1,14 @@
-def first_uniq_character(str):
-    freq_hash = {}
-    for char in str:
-        if freq_hash.get(char) is not None:
-            freq_hash[char] += 1
-        else:
-            freq_hash[char] = 1
-
-    uniq_set = {char for char in freq_hash.keys() if freq_hash[char] == 1}
+def get_first_uniq_char(str):
+    first_uniq = len(str)
+    table = {}
     for i, char in enumerate(str):
-        if char in uniq_set:
-            return i
+        if table.get(char) is not None:
+            table[char] = -1
+        else:
+            table[char] = i
 
-    return -1
+    for key, val in table.items():
+        if val != -1 and first_uniq > val:
+            first_uniq = val
+
+    return first_uniq if first_uniq < len(str) else -1
