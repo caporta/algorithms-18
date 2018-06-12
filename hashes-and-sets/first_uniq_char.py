b459b4bb4aa076c27a089451c0159372677dd901
caporta <caporta@users.noreply.github.com>
2018-06-12 19:27:21 -0400

    Improve clarity

--- hashes-and-sets/first_uniq_char.py
+++ hashes-and-sets/first_uniq_char.py
@@ -1,8 +1,8 @@
 def get_first_uniq_char(str):
-    first_uniq = len(str)
+    first_uniq = float('inf')
     table = {}
     for i, char in enumerate(str):
-        if table.get(char) is not None:
+        if char in table:
             table[char] = -1
         else:
             table[char] = i
@@ -11,4 +11,4 @@ def get_first_uniq_char(str):
         if val != -1 and first_uniq > val:
             first_uniq = val
 
-    return first_uniq if first_uniq < len(str) else -1
+    return first_uniq if first_uniq < float('inf') else -1
